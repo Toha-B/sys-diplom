aa9b9e04d48c0e3754f75d5b103072cb833665bf
Anton <121480778+Toha-B@users.noreply.github.com>
2023-10-15 15:30:38 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,3 @@
-# Переменные
-
 terraform {
   required_providers {
     yandex = {
@@ -9,21 +7,112 @@ terraform {
   required_version = ">= 0.13"
 }
 
-# Провайдер
-
 provider "yandex" {
   token        = "xxx"
   cloud_id     = "xxx"
   folder_id    = "xxx"
 }
 
-# Создание сети
+#=====================================================
+
+# Security bastion host
+
+resource "yandex_vpc_security_group" "group-bastion-host" {
+  name        = "My security group bastion host"
+  network_id  = yandex_vpc_network.network-1.id
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    from_port      = 0
+    to_port        = 65535
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security group to allow incoming ssh traffic
+resource "yandex_vpc_security_group" "group-ssh-traffic" {
+  name        = "My security group ssh traffic"
+  network_id  = yandex_vpc_network.network-1.id
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
+  }
+
+  ingress {
+    protocol       = "ICMP"
+    from_port      = 0
+    to_port        = 65535
+    v4_cidr_blocks = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
+  }
+}
+
+# Security group webservers
+resource "yandex_vpc_security_group" "group-webservers" {
+  name        = "My security group webservers"
+  network_id  = yandex_vpc_network.network-1.id
+
+  ingress {
+    protocol       = "TCP"
+    port           = 80
+   # predefined_target = "loadbalancer_healthchecks"
+    v4_cidr_blocks = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    port           = 4040
+    v4_cidr_blocks = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    port           = 9100
+    v4_cidr_blocks = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
+
+# Security group public application load balancer
+resource "yandex_vpc_security_group" "group-public-network-alb" {
+  name        = "My security group public network application load balancer"
+  network_id  = yandex_vpc_network.network-1.id
+
+  ingress {
+    protocol       = "TCP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
+
+#=======================================================
+
+# network
 
 resource "yandex_vpc_network" "network-1" {
   name = "network1"
 }
 
-# Создание подсетей
+# subnet
 
 resource "yandex_vpc_subnet" "subnet1" {
   name = "subnet1"
@@ -31,14 +120,12 @@ resource "yandex_vpc_subnet" "subnet1" {
   network_id = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
 resource "yandex_vpc_subnet" "subnet2" {
   name = "subnet2"
   zone = "ru-central1-b"
   network_id = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.20.0/24"]
 }
-
 resource "yandex_vpc_subnet" "subnet3" {
   name = "subnet3"
   zone = "ru-central1-c"
@@ -46,11 +133,43 @@ resource "yandex_vpc_subnet" "subnet3" {
   v4_cidr_blocks = ["192.168.30.0/24"]
 }
 
-#=======================
-#Создание vm1, vm2
+# bastion host
 
-resource "yandex_compute_instance" "web-server1" {
-  name = "web-server1"
+resource "yandex_compute_instance" "bastion-host" {
+
+  name = "bastion-host"
+  zone = "ru-central1-c"
+
+  resources {
+    cores = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80iibe8asp4inkhuhr"
+      size = 13
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet3.id
+    nat       = true
+    security_group_ids = [yandex_vpc_security_group.group-bastion-host.id]
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yaml")}"
+  }
+}
+
+#===================================================
+
+#web-server-1
+
+resource "yandex_compute_instance" "nginxserver1" {
+  name = "nginxserver1"
   zone = "ru-central1-a"
 
   resources{
@@ -63,7 +182,7 @@ resource "yandex_compute_instance" "web-server1" {
     initialize_params {
       image_id = "fd8o41nbel1uqngk0op2"
       size = 10
-      description = "boot disk for web_server1"
+      description = "boot disk for nginx_server1"
     }
   }
   network_interface {
@@ -71,12 +190,14 @@ resource "yandex_compute_instance" "web-server1" {
     nat = true
   }
   metadata = {
-    user-data = "${file("./meta.yml")}"
+    user-data = "${file("./meta.yaml")}"
   }
 }
 
-resource "yandex_compute_instance" "web-server2" {
-  name = "web-server2"
+#web-server-2
+
+resource "yandex_compute_instance" "nginxserver2" {
+  name = "nginxserver2"
   zone = "ru-central1-b"
 
   resources{
@@ -84,11 +205,12 @@ resource "yandex_compute_instance" "web-server2" {
     core_fraction = 20
     memory = 4
   }
- boot_disk{
+
+  boot_disk{
     initialize_params {
       image_id = "fd8o41nbel1uqngk0op2"
       size = 10
-      description = "boot disk for web_server1"
+      description = "boot disk for nginx_server1"
     }
   }
   network_interface {
@@ -96,6 +218,114 @@ resource "yandex_compute_instance" "web-server2" {
     nat = true
   }
   metadata = {
-    user-data = "${file("./meta.yml")}"
+    user-data = "${file("./meta.yaml")}"
   }
 }
+
+#===================================================
+
+# target group
+
+resource "yandex_alb_target_group" "my-target-group" {
+  name      = "my-target-group"
+  #region_id = "ru-central1"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet1.id}"
+    ip_address   = "${yandex_compute_instance.nginxserver1.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet2.id}"
+    ip_address   = "${yandex_compute_instance.nginxserver2.network_interface.0.ip_address}"
+  }
+}
+
+# Backend
+
+resource "yandex_alb_backend_group" "my-backend-group" {
+  name      = "my-backend-group"
+
+  http_backend {
+    name = "my-http-backend"
+    weight = 1
+    port = 80
+    target_group_ids = ["${yandex_alb_target_group.my-target-group.id}"]
+    healthcheck {
+      timeout = "1s"
+      interval = "1s"
+      http_healthcheck {
+        path  = "/"
+      }
+    }
+  }
+}
+
+#=======================================================================
+
+# HTTP router
+
+resource "yandex_alb_http_router" "my-http-router" {
+  name      = "my-http-router"
+}
+
+#=======================================================================
+
+# hosts
+
+resource "yandex_alb_virtual_host" "my-virtual-host" {
+  name      = "my-virtual-host"
+  http_router_id = yandex_alb_http_router.my-http-router.id
+  route {
+    name = "my-route"
+
+    http_route {
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.my-backend-group.id
+        timeout = "3s"
+      }
+    }
+  }
+}
+
+#======================================================================
+
+# networkloadbalancer
+
+resource "yandex_alb_load_balancer" "my-network-load-balancer" {
+  name        = "my-load-balancer"
+
+  network_id  = yandex_vpc_network.network-1.id
+  security_group_ids = [yandex_vpc_security_group.group-public-network-alb.id]
+
+  allocation_policy {
+    location {
+      zone_id   = "ru-central1-a"
+      subnet_id = yandex_vpc_subnet.subnet1.id
+    }
+
+    location {
+      zone_id   = "ru-central1-b"
+      subnet_id = yandex_vpc_subnet.subnet2.id
+    }
+  }
+
+  listener {
+    name = "my-listener"
+    endpoint {
+      address {
+        external_ipv4_address {
+        }
+      }
+      ports = [ 80 ]
+    }
+    http {
+      handler {
+        http_router_id = yandex_alb_http_router.my-http-router.id
+      }
+    }
+  }
+}
+
+
+
